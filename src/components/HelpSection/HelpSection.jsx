6740a312b27ea4065f375d0bee0eed5105ec1538
Vaibhav Jumde <82874349+vaibhav209@users.noreply.github.com>
2023-05-19 21:02:48 +0530

    Update HelpSection.jsx
    update text styles

--- src/components/HelpSection/HelpSection.jsx
+++ src/components/HelpSection/HelpSection.jsx
@@ -4,13 +4,11 @@ import { useNavigate } from 'react-router-dom';
 import routes from '../../routes/routes.json';
 
 const HelpSection = () => {
-
-
   const navigate = useNavigate();
 
-  const playButtonHandle = ()=>{
-    navigate(routes.USERHOME)
-  }
+  const playButtonHandle = () => {
+    navigate(routes.USERHOME);
+  };
 
   return (
     <div className={styles.helpContainer}>
@@ -18,8 +16,9 @@ const HelpSection = () => {
         <li>
           <h3>Create Color Combination:</h3>
           <p className={styles.textStyles}>
-            Click "Select Color" to open the color palette. Adjust the RGB
-            values using sliders.
+            Click "Select Color" to open the color palette.
+            <br />
+            Adjust the RGB values using sliders.
             <br />
             Click "Save" to save the color to your collection.
           </p>
@@ -29,8 +28,9 @@ const HelpSection = () => {
           <p className={styles.textStyles}>
             Click on a color box to select it.
             <br />
-            The color box will become the background color. The selected color
-            will be removed from your collection.
+            The color box will become the background color.
+            <br />
+            The selected color will be removed from your collection.
           </p>
         </li>
         <li className={styles.listStyle}>
@@ -43,7 +43,9 @@ const HelpSection = () => {
         </li>
       </ul>
       <div className={styles.buttonBox}>
-      <button className={styles.playButton} onClick={playButtonHandle}>&#8592; Continue to Play</button>
+        <button className={styles.playButton} onClick={playButtonHandle}>
+          &#8592; Continue to Play
+        </button>
       </div>
     </div>
   );
