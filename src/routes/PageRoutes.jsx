587907bbe7b2d31f404559a44b40599eae386d6d
Vaibhav Jumde <82874349+vaibhav209@users.noreply.github.com>
2023-05-19 15:46:30 +0530

    Update PageRoutes.jsx
    update help feature path

--- src/routes/PageRoutes.jsx
+++ src/routes/PageRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Error from '../pages/Error';
 import Feedback from '../pages/Feedback';
+import Help from '../pages/Help';
 import Home from '../pages/Home';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -16,7 +17,7 @@ const PageRoutes = () => {
         <Route path={routes.USERHOME} element={<Home />} />
         <Route path={routes.HELP} element={<Help />} />
       </Route>
-
+      
       <Route path={routes.USERSIGNIN} element={<SignIn />} />
       <Route path={routes.USERSIGNUP} element={<SignUp />} />
       <Route path={routes.USERFEEDBACK} element={<Feedback />} />
