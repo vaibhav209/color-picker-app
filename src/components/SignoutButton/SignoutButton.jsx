e5794953c3556c4c1f28439ad3d8df771d7edac8
vaibhav jumde <vaibhavjumde97@gmail.com>
2023-09-01 18:09:38 +0530

    chore: remove unused code

--- src/components/SignoutButton/SignoutButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './SignoutButton.module.css';
-
-const SignoutButton = ({onClick}) => {
-  return <button className={styles.signoutStyles} onClick={onClick}>Sign Out</button>;
-};
-
-export default SignoutButton;
